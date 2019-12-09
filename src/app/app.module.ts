9d89e6ead6da6d8d144d25c800f78d08c21f46b2
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-12-08 20:55:22 -0600

    generated top-bar and menu components, added styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
