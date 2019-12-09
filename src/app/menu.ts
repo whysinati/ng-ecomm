f70583c6707e0ed9b0011af8aa11d9fbcd15640e
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-12-09 10:36:23 -0600

    Menu updates

---
+++ src/app/menu.ts
@@ -0,0 +1,31 @@
+export const menu = [
+    {
+        name: 'Carne TexMex',
+        type: 'Entree',
+        price: 14.99,
+        description: 'Juicy seared skirt steak marinated in ancho chile, cumin, coriander, lime and cilantro'
+    },
+    {
+        name: 'Rosemary Lemon Roasted Brussels Sprouts',
+        type: 'Side',
+        price: 6.99,
+        description: 'A little char, a little caramelization... delightful!'
+    },
+    {
+        name: 'Not *Just* Coffee',
+        type: 'Barista',
+        price: 4.99,
+        description: 'Bright pourover from our favorite medium roast beans.'
+    },
+    {
+        name: 'Water',
+        type: 'Barista',
+        price: 0,
+        description: 'Tap water. The original healthy drink.'
+    },
+    {
+        name: 'Sparkling Water',
+        type: 'Barista',
+        price: 1.99
+    }
+]
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { menu } from '../menu';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent { // implements OnInit {
+  menu = menu;
 
-  constructor() { }
-
-  ngOnInit() {
+  share() {
+    window.alert('This menu item has been shared!');
   }
+
+  // constructor() { }
+
+  // ngOnInit() {
+  // }
 
 }
